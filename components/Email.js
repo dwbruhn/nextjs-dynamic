2d628bade2bde622e06a75ee307d9c6903a8bb33
Bruhn, Daniel <dbruhn@paypal.com>
2022-02-04 12:47:06 -0500

    feat: add dynamically imported email field

---
+++ components/Email.js
@@ -0,0 +1,16 @@
+const Email = () => {
+  return (
+    <div>
+      <label htmlFor="email">Email Address:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        required
+        placeholder="sophie@example.com"
+      />
+    </div>
+  )
+}
+
+export default Email
